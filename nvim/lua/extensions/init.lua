0e4261d36e5cb8657fd79d487398ca82f6b1d9d7
staakaar <vancyroletaka@gmail.com>
2026-02-22 18:09:52 +0900

    add(nvim): a number of plugins

--- nvim/lua/extensions/init.lua
+++ nvim/lua/extensions/init.lua
@@ -1,4 +1,69 @@
-local plugins = {}
+local plugins = {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    event = { 'BufNewFile', 'BufReadPre' },
+  },
+  {
+    'rmehri01/onenord.nvim',
+    event = { 'VimEnter' },
+    priority = 1000,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    event = { 'VimEnter' },
+    config = function() require 'extensions.lualine' end,
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+  {
+    'kevinhwang91/nvim-hlslens',
+    event = { 'FilterWritePre' },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre' },
+  },
+  {
+    'petertriho/nvim-scrollbar',
+    event = { 'BufNewFile', 'BufReadPre' },
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    config = function() require 'extensions.oil' end,
+    lazy = false,
+  },
+  {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    branch = "stable",
+    lazy = false,
+    opts = {
+      integrations = {
+        icon = "nvim_web_devicons",
+      }
+    },
+    keys = {
+      { "<leader>e", "<Cmd>Fyler<Cr>", desc = "Open Fyler View" },
+    }
+  }
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   version = "*",
+  --   lazy = false,
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   config = function()
+  --     require("nvim-tree").setup {}
+  --   end,
+  -- }
+}
 
 local opts = {
   checker = {
